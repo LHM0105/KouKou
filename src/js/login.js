866629bc573a5c5fc792f3326e8fc5e5352dee45
lhm <1798688440@qq.com>
2017-10-08 15:41:58 +0800

    登录页完成

--- src/js/login.js
+++ src/js/login.js
@@ -9,10 +9,15 @@ require.config({
 require(['jquery'], function ($) {
 
 	$(function () {
-		//加载底部
-		$('#footer').load('index.html #footer .footer-con','login',function(){});
-		$('#footer-about').load('index.html #footer-about .foot-about-con','login',function(){});
-		$('#footer-copy').load('index.html #footer-copy .foot-copy-con','login',function(){});
+		//用户名输入框
+		$('#username').on('focus',function(){
+			$(this).val('').css('color','#000');
+			
+		});
+		$('#username').on('blur',function(){
+			$(this).val('邮箱/用户名/手机号').css('color','#555');
+			
+		});
 	});
 	
 });
