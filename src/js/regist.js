a1e3e95475f5dde5eff4453e0ee4f658831129ba
lhm <1798688440@qq.com>
2017-10-08 22:23:18 +0800

    注册页面html,css

--- src/js/regist.js
+++ src/js/regist.js
@@ -17,6 +17,24 @@ require(['jquery'], function ($) {
 		$('#footer-copyright').load('login.html #footer-copyright p');
 		$('#footer-icon').load('login.html #footer-icon a');
 		
+		//注册表单部分
+		//协议部分
+		$('form .xieyi').on('click','label',function(){
+			$('form .xieyi label').toggleClass('checked');
+		});
+		
+		//设置遮罩层高度
+		$('.maskbg').height($('body').height());
+		//协议弹出窗口
+		$('form .xieyi').on('click','a',function(){
+			$('.maskbg').css('display','block');
+			$('.xieyi-box').css('display','block');
+		});
+		//点击弹出窗的关闭或者“同意并继续”，弹出窗消失
+		$('.xieyi-box .close').on('click',function(){
+			$('.xieyi-box').css('display','none');
+			$('.maskbg').css('display','none');
+		});
 		
 	});
 	
