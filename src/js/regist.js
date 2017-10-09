8c396a51cfa67cfce6f462eaa5b61e6cc435a777
lhm <1798688440@qq.com>
2017-10-09 11:29:13 +0800

    密码强度验证完成，注册页面完成

--- src/js/regist.js
+++ src/js/regist.js
@@ -112,13 +112,89 @@ require(['jquery'], function ($) {
 		//密码
 		// 密码强弱
 		
-//		require(['js/jquery.passwordStrength'], function ($) {
-//			$(document).ready(function(){
-//			    var $pwd = $('input[name="password"]');
-//			    $pwd.passwordStrength();
-//			});
-//		});
-
+		//密码输入框值改变时
+		$('.mima #psw').on('input',function(){
+			var H = $('.mima #psw').val();
+			var D = H.length;//密码长度
+	        var E;
+	        if(D == 1){ // 输入一个字符的时候
+	            E=-13;    // 显示一个 强度
+	        }
+	        if(D==2){  // 输入2个字符的时候
+	            E=-27;  // 显示2个 强度
+	        }
+	        if(D==3){  // 输入2个字符的时候
+	            E=-40;  // 显示3个 强度
+	        }
+	
+	        if(D>3 && D<7){  // 输入 3到 7 之间
+	            E=-53;        // 5个 强度
+	        }
+	        var num =  /^\d+$/;
+	        if(num.test(H)  && D>=7){ // 纯数字 时
+	            E=-53;  // 5个 强度
+	            // $('.p_tishi2').show();
+	        }
+	        var zm = /^[A-Za-z]+$/;
+	        if(zm.test(H)  &&  D>=7){  // 纯字母 时
+	
+	            E=-53;  // 5个 强度
+	        }
+	        var zh = /[A-Za-z]+[0-9]+/;
+	        if(zh.test(H)  &&  D>=7){   // 字母 加 数字  时
+	            E=-90;
+	        }
+	        var n_z = /[0-9]+[A-Za-z]+/;
+	        if(n_z.test(H)  &&  D>=7){  //  数字 加 字母  时
+	            E=-90;
+	        }
+	        //  字母 + 特殊符号 +  字母
+	        var z_m = /(^[A-Za-z]+)([^A-Za-z0-9]+)([A-Za-z]+)$/;
+	
+	        if(z_m.test(H) && D>=7){
+	           E=-110;
+	        }
+	        //  字母 + 特殊符号
+	
+	        var t_s_z = /(^[A-Za-z]+)([^A-Za-z0-9]+)$/
+	
+	        if(t_s_z.test(H)  && D>=7 ){
+	            E=-116;
+	        }
+	
+	        //  数字 + 特殊符号 +  数字
+	        var sz_t = /(^[\d]+)([^A-Za-z0-9]+)([\d]+)$/;
+	        if(sz_t.test(H)  && D>=7 ){
+	            E=-130
+	        }
+	
+	        //  数字 + 特殊符号
+	        var sz_ts = /(^[\d]+)([^A-Za-z0-9]+)$/;
+	
+	        if(sz_ts.test(H)  && D>=7 ){
+	            E=-90;
+	        }
+	
+	        // 特殊符号 +  数字
+	        var ts_sz = /(^[^A-Za-z0-9]+)([\d]+)$/
+	
+	        if(ts_sz.test(H)  && D>=7){
+	            E=-103
+	        }
+	        // 特殊符号 +  字母
+	        var ts_zu = /(^[^A-Za-z0-9]+)([A-Za-z]+)$/;
+	        if(ts_zu.test(H)  && D>=7){
+	            E=-116
+	        }
+	        // 字母 + 数字 + 特殊符号
+	        var gh = /(?=.*[\d]+)(?=.*[A-Za-z]+)(?=.*[^A-Za-z0-9]+)/;
+		    if(gh.test(H)  && D>=7){
+		        E=-130
+		    }
+			
+			$('.pswStrenthDiv').css('background-position-y',E);
+		});
+		
 		//确认密码输入框默认不可用
 		$('.psw-sure #psw-s').prop('disabled',true);
 		
@@ -127,6 +203,7 @@ require(['jquery'], function ($) {
 		$('.mima #psw').focus(function(){
 			$('.mima .tishi').css('display','none');
 			$('.mima .info').css('display','block');
+			$(this).css('border-color','#d0d0d0');
 		});
 		
 		//失去焦点
