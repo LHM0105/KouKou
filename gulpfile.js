4b6dd7c1a586ede51b50495c97f0f3c25c3d403c
lhm <1798688440@qq.com>
2017-10-11 14:23:46 +0800

    购物车页面样式完成，完成模拟加载数据，部分js效果未加

--- gulpfile.js
+++ gulpfile.js
@@ -26,14 +26,14 @@ gulp.task('rename',function(){
 
 //新建压缩重命名js文件的任务
 gulp.task('minifyJS',function(){
-	return gulp.src('./src/js/goods.js').
+	return gulp.src('./src/js/shopcart.js').
 	pipe(uglify()).
 	pipe(gulp.dest('./dist/js'));
 });
 
 //新建一个压缩html文件的任务(不压缩也可以，主要是压缩css和js)
 gulp.task('minifyHTML',function(){
-	return gulp.src('./src/goods.html')
+	return gulp.src('./src/shopcart.html')
 	.pipe(minifyhtml({
 		collapseWhitespace: true
 	}))
@@ -41,7 +41,7 @@ gulp.task('minifyHTML',function(){
 });
 //操作sass文件并且压缩到dist中
 gulp.task('minifySCSS',function(){
-	return sass('./src/scss/goods.scss',{
+	return sass('./src/scss/shopcart.scss',{
 		style: 'compressed'
 	}).pipe(minifycss())
 	.pipe(gulp.dest('./dist/css'));
@@ -49,7 +49,7 @@ gulp.task('minifySCSS',function(){
 
 //把scss转为css文件(放在源文件中)
 gulp.task('compileSCSS',function(){
-	return sass('./src/scss/goods.scss',{
+	return sass('./src/scss/shopcart.scss',{
 		style: 'expanded'
 	}).pipe(gulp.dest('./src/css'));
 });
@@ -79,5 +79,5 @@ gulp.task('default',["minifyJS","minifySCSS",'minifyHTML','compileSCSS'],functio
 		livereload:true
 	});
 
-	gulp.watch(['./src/js/goods.js','./src/scss/goods.scss','./src/goods.html','./src/scss/goods.scss'],['reload']);
+	gulp.watch(['./src/js/shopcart.js','./src/scss/shopcart.scss','./src/shopcart.html','./src/scss/shopcart.scss'],['reload']);
 });
---
+++ src/js/shopcart.js
@@ -0,0 +1,56 @@
+require.config({
+	// 定义路径
+	baseUrl: './',
+	paths: {
+		'jquery': 'js/jquery1.8.3',
+		'mock': 'http://mockjs.com/dist/mock'
+	}
+});
+
+require(['jquery'], function ($) {
+	//从登陆页面加载内容
+	$('#top').load('login.html #top .top-con');
+	$('#footer-service').load('login.html #footer-service .service-con');
+	$('#footer-copyright').load('login.html #footer-copyright p');
+	$('#footer-icon').load('login.html #footer-icon a');
+	$('#footer-out').load('login.html #footer-out .footer');
+	
+	//模拟数据//加载mock
+		require(['mock'], function (Mock) {
+			
+			Mock.mock('http://g.cn', {
+				'goods|5': [{	//5件商品
+			        'id|1-5': 1,//id从1开始，每次自增1
+			        'price|30-200' : 20,//30-200之间
+			        'info' : '商品的描述信息一些描述信息商品的描述信息一些描述信息',
+			        'shop' : '店铺名',
+			        'size' : '155/S/95',
+			        'nowPrice|100-200' : 10,
+			        'oldPrice|200-300' : 10,
+			        'num|1-10' : 1,//商品数量 
+			        'color' : '@color'
+			    }]
+			});
+			//加载数据
+			$.ajax({
+			    url: 'http://g.cn',
+			    dataType:'json'
+			   }).done(function(data, status, xhr){
+			    console.log(data.goods);
+				
+		      	//显示商品信息
+			    for( var i = 0;i < data.goods.length;i++){
+			    	//图片地址
+			    	var src = data.goods[i].id + ".jpg";
+//			    	var price = data.goods[i].price;
+			    	var obj = document.createElement('table');
+//			    	obj.className = 'goods-box';
+			    	obj.innerHTML = '<table><thead><tr><td colspan="8"><input type="checkbox" checked="checked" class="chose" name="chose" id="chose" /><a href="#">'+ data.goods[i].shop +'</a></td></tr></thead><tbody><tr><td class="ischeck"><input type="checkbox" name="ischecked" checked="checked" value="" id=""/></td><td class="goods-info"><dl><dt><a href="#"><img src="img/'+ src +'" /></a></dt><dd><p class="info-text">'+ data.goods[i].info +'</p><p class="bz"><span class="jian"></span><span class="zhen"></span><span class="zheng"></span></p></dd></dl></td><td class="color-size"> <span>尺码：<b>'+ data.goods[i].size +'</b></span> <span>颜色分类：<b>'+ data.goods[i].color +'</b></span> </td> <td class="price"><span class="now-p">￥<b>'+ data.goods[i].nowPrice +'</b></span><span class="old-p">￥<b>'+ data.goods[i].oldPrice+'</b></span></td><td class="goods-num"><div class="num-box"><span class="differ"></span><input type="text" value="1" name="goods-num" class="goos-num"/><span class="add"></span></div></td><td class="cx"><p>满100减30</p></td><td class="price0"><span>￥<b>'+ data.goods[i].nowPrice +'</b></span></td><td class="cz"><a href="#" class="move">移入收藏夹</a><a href="#" class="delete">删除</a></td></tr></tbody></table>';
+			    	//追加到页面
+			    	$('#shopcart .shopcart-con .shopcart-con0').append(obj);
+			    }
+			});
+			
+			
+		});
+});
